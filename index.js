94d6e52ae0d443b8759e91617377529c8a91d92b
devland <adevland@gmail.com>
2026-04-30 00:26:33 +0300

    update sample and clean up structure

--- index.js
+++ index.js
@@ -2,14 +2,35 @@ const { spawn } = require("child_process");
 module.exports = (args) => {
   return new Promise((dbResolve, dbReject) => {
     try {
+      const sql = {
+        run: (query) => {
+          data = '';
+          return new Promise((resolve, reject) => {
+            queryResolve = resolve;
+            queryReject = reject;
+            shell.stdin.write(query);
+          });
+        },
+        end: () => {
+          return new Promise((resolve, reject) => {
+            queryResolve = resolve;
+            queryReject = reject;
+            shell.stdin.end();
+          });
+        }
+      }
       let output = '';
       let data = '';
-      let sql = {};
+      let errorData = '';
       let queryResolve;
       let queryReject;
       const handle = (error, result) => {
-        if (error && typeof queryReject == 'function') queryReject(error);
-        else if (typeof queryResolve == 'function') queryResolve(result);
+        if (error && typeof queryReject == 'function') {
+          queryReject(error);
+        }
+        else if (typeof queryResolve == 'function') {
+          queryResolve(result);
+        }
         queryReject = null;
         queryResolve = null;
       }
@@ -22,9 +43,9 @@ module.exports = (args) => {
           output = JSON.parse(data);
           handle(null, output);
         }
-        catch(error) {
+        catch (error) {
           // JSON.parse error due to partial data; safe to ignore;
-          //console.log(error);
+          // console.log(error);
         }
       });
       shell.stderr.on('data', (chunk) => {
@@ -33,24 +54,9 @@ module.exports = (args) => {
       shell.on('close', (code) => {
         handle(null, code);
       });
-      sql.run = (query) => {
-        data = '';
-        return new Promise((resolve, reject) => {
-          queryResolve = resolve;
-          queryReject = reject;
-          shell.stdin.write(query);
-        });
-      }
-      sql.end = () => {
-        return new Promise((resolve, reject) => {
-          queryResolve = resolve;
-          queryReject = reject;
-          shell.stdin.end();
-        });
-      }
       dbResolve(sql);
     }
-    catch(error) {
+    catch (error) {
       dbReject(error);
     }
   });
--- sample/test.js
+++ sample/test.js
@@ -3,12 +3,13 @@ let sql;
 shell(['db.sqlite3']) // sqlite3 db to use; other sqlite3 cli arguments can be provided as array items
   .then((result) => {
     sql = result;
-    return sql.run('SELECT random() FROM generate_series(1,2000);\n');
+    return sql.run('SELECT random() FROM generate_series(1, 2000);\n');
   })
   .then((result) => {
     console.log('>>> sql run 1 result');
     console.log(result);
-    return sql.run('select * from users;\n');
+    return sql.run(`.parameter set :name "admin"\n
+      select * from users where name = :name;\n`);
   })
   .then((result) => {
     console.log('>>> sql run 2 result');
@@ -22,4 +23,5 @@ shell(['db.sqlite3']) // sqlite3 db to use; other sqlite3 cli arguments can be p
   .catch((error) => {
     console.log('>>> query error');
     console.log(error);
+    process.exit(1);
   });
