0c8bcff644b7ba8bcfc82326b28c383978b6be45
devland <adevland@gmail.com>
2026-05-19 19:56:47 +0300

    add named params support for queries; update sample queries (v1.1.0)

--- index.js
+++ index.js
@@ -1,63 +1,64 @@
 const { spawn } = require("child_process");
-module.exports = (args) => {
-  return new Promise((dbResolve, dbReject) => {
+module.exports = function (args) {
+  let output = '';
+  let data = '';
+  let errorData = '';
+  let queryResolve;
+  let queryReject;
+  const handle = (error, result) => {
+    if (error && typeof queryReject == 'function') {
+      queryReject(error);
+    }
+    else if (typeof queryResolve == 'function') {
+      queryResolve(result);
+    }
+    queryReject = null;
+    queryResolve = null;
+  }
+  const computeParams = (map = []) => {
+    let params = '';
+    for (let key of Object.keys(map)) {
+      if (typeof map[key] == 'undefined') {
+        continue;
+      }
+      params += `.parameter set :${key} "${map[key]}"\n`;
+    }
+    return params;
+  }
+  this.run = (query, map) => {
+    data = '';
+    return new Promise((resolve, reject) => {
+      queryResolve = resolve;
+      queryReject = reject;
+      const params = computeParams(map);
+      shell.stdin.write(params + query + ';\n');
+    });
+  }
+  this.end = () => {
+    return new Promise((resolve, reject) => {
+      queryResolve = resolve;
+      queryReject = reject;
+      shell.stdin.end();
+    });
+  }
+  shell = spawn('sqlite3', ['-json'].concat(args));
+  shell.stdout.setEncoding('utf8');
+  shell.stderr.setEncoding('utf8');
+  shell.stdout.on('data', (chunk) => {
+    data += chunk;
     try {
-      const sql = {
-        run: (query) => {
-          data = '';
-          return new Promise((resolve, reject) => {
-            queryResolve = resolve;
-            queryReject = reject;
-            shell.stdin.write(query);
-          });
-        },
-        end: () => {
-          return new Promise((resolve, reject) => {
-            queryResolve = resolve;
-            queryReject = reject;
-            shell.stdin.end();
-          });
-        }
-      }
-      let output = '';
-      let data = '';
-      let errorData = '';
-      let queryResolve;
-      let queryReject;
-      const handle = (error, result) => {
-        if (error && typeof queryReject == 'function') {
-          queryReject(error);
-        }
-        else if (typeof queryResolve == 'function') {
-          queryResolve(result);
-        }
-        queryReject = null;
-        queryResolve = null;
-      }
-      shell = spawn('sqlite3', ['-json'].concat(args));
-      shell.stdout.setEncoding('utf8');
-      shell.stderr.setEncoding('utf8');
-      shell.stdout.on('data', (chunk) => {
-        data += chunk;
-        try {
-          output = JSON.parse(data);
-          handle(null, output);
-        }
-        catch (error) {
-          // JSON.parse error due to partial data; safe to ignore;
-          // console.log(error);
-        }
-      });
-      shell.stderr.on('data', (chunk) => {
-        handle(chunk);
-      });
-      shell.on('close', (code) => {
-        handle(null, code);
-      });
-      dbResolve(sql);
+      output = JSON.parse(data);
+      handle(null, output);
     }
     catch (error) {
-      dbReject(error);
+      // JSON.parse error due to partial data; safe to ignore;
+      // console.log(error);
     }
+  });
+  shell.stderr.on('data', (chunk) => {
+    handle(chunk);
+  });
+  shell.on('close', (code) => {
+    handle(null, code);
   });
 }
--- sample/test.js
+++ sample/test.js
@@ -1,27 +1,53 @@
 const shell = require('../index.js');
-let sql;
-shell(['db.sqlite3']) // sqlite3 db to use; other sqlite3 cli arguments can be provided as array items
-  .then((result) => {
-    sql = result;
-    return sql.run('SELECT random() FROM generate_series(1, 2000);\n');
-  })
-  .then((result) => {
-    console.log('>>> sql run 1 result');
-    console.log(result);
-    return sql.run(`.parameter set :name "admin"\n
-      select * from users where name = :name;\n`);
-  })
-  .then((result) => {
-    console.log('>>> sql run 2 result');
-    console.log(result);
-    return sql.end(); // ends shell process
-  })
-  .then((result) => {
-    console.log('>>> shell end result');
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log('>>> query error');
-    console.log(error);
-    process.exit(1);
+let sql = new shell(['db.sqlite3']);
+let benchStart;
+let benchEnd;
+benchStart = performance.now();
+sql.run('select random() from generate_series(1, 2000);\n')
+.then((result) => {
+  benchEnd = performance.now();
+  console.log(`>>> sql random() result done in ${benchEnd - benchStart} ms`);
+  console.log(result);
+  benchStart = performance.now();
+  return sql.run('select * from users where name = :name', { name: 'admin' });
+})
+.then((result) => {
+  benchEnd = performance.now();
+  console.log(`>>> sql named parameter result done in ${benchEnd - benchStart} ms`);
+  console.log(result);
+  benchStart = performance.now();
+  /* WARNING!!!
+   * Since write queries like insert, update and delete do not output anything in the sqlite3 cli
+   * remember to follow them with a select query
+   * so that the shell stdout has something to print
+   * so that the promise can resolve */
+  return sql.run(`
+insert into users (name, key) values (:name, :key);
+select * from users where name = :name`, {
+    name: 'new_user',
+    key: 'some key 123'
   });
+})
+.then((result) => {
+  benchEnd = performance.now();
+  console.log(`>>> sql insert & select done in ${benchEnd - benchStart} ms`);
+  console.log(result);
+  benchStart = performance.now();
+  return sql.run(`
+delete from users where name = :name;
+select count(*) as user_count from users`, { name: 'new_user' });
+})
+.then((result) => {
+  benchEnd = performance.now();
+  console.log(`>>> sql delete & select done in ${benchEnd - benchStart} ms`);
+  console.log(result);
+  return sql.end(); // ends shell process
+})
+.then((result) => {
+  console.log(result == 0 ? '>>> done' : 'shell not closed...');
+})
+.catch((error) => {
+  console.log('>>> query error');
+  console.log(error);
+  process.exit(1);
+});
